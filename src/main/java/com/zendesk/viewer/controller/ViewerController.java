fd23358cf0189d3b84b17650262f0ed4a5bc843b
nonconformist811 <86326313+nonconformist811@users.noreply.github.com>
2021-11-29 01:54:05 -0500

    some comments added

--- src/main/java/com/zendesk/viewer/controller/ViewerController.java
+++ src/main/java/com/zendesk/viewer/controller/ViewerController.java
@@ -12,11 +12,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.io.IOException;
 import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
+/**
+ * @author sumbulfatima
+ * Zendesk Ticket Viewer Controller
+ */
 
 @Controller
 public class ViewerController {
